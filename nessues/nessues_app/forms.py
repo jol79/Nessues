7e0e14c407b4edb3bf117238d861b39499d384a2
jol79 <jolhuck@gmail.com>
2021-04-16 10:41:53 +0300

    close #22

--- nessues/nessues_app/forms.py
+++ nessues/nessues_app/forms.py
@@ -1,13 +1,22 @@
 from django import forms
 from django.forms import CharField, HiddenInput
-from .models import Room
+from .models import Room, Table
 
 
 class CreateRoomForm(forms.ModelForm):
     name = CharField(max_length=21)
-    description = CharField(max_length=60)
+    description = CharField(max_length=32)
 
     class Meta:
         model = Room
         fields = ['name', 'description', 'owner']
         widgets = {'owner': forms.HiddenInput()}
+
+class CreateTableForm(forms.ModelForm):
+    name = CharField(max_length=21)
+    description = CharField(max_length=60)
+
+    class Meta:
+        model = Table
+        fields = ['name', 'description', 'room', 'group']
+        widgets = {'room': forms.HiddenInput(), 'group': forms.HiddenInput()}
--- nessues/nessues_app/views.py
+++ nessues/nessues_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from .models import Room, Table, Task
-from .forms import CreateRoomForm
+from .forms import CreateRoomForm, CreateTableForm
 
 
 def home_view(request):
@@ -31,9 +31,16 @@ def rooms_view(request):
     return render(request, 'nessues_app/mono_rooms.html', {'title': title, 'content': content})
 
 def tables_view(request, key_id):
+    form = CreateTableForm(initial={'room': key_id})
+    if request.method == "POST":
+        form = CreateTableForm(request.POST)
+        if form.is_valid():
+            form.save()
+
     content = {
         'available_tables': Table.objects.filter(room=key_id),
-        'room': Room.objects.filter(id=key_id)
+        'room': Room.objects.filter(id=key_id),
+        'form': form
     }
 
     title = "tables"
