4b37b4761e24860fec37269c844f44e3c98b2197
jol79 <jolhuck@gmail.com>
2021-04-22 19:54:44 +0300

    close #33

--- nessues/nessues_app/views.py
+++ nessues/nessues_app/views.py
@@ -56,23 +56,6 @@ def tables_view(request, key_id):
     return render(request, 'nessues_app/tables.html', {'title': title, 'content': content})
 
 
-# def tasks_view(request, key_id):
-#     form = CreateTaskForm(initial={'table': key_id, 'created_by': request.user.id})
-#     if request.method == "POST":
-#         form = CreateTaskForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             form = CreateTaskForm()
-#             return HttpResponseRedirect(f'/tables/tasks/{key_id}')
-
-#     content = {
-#         'available_tasks': Task.objects.filter(table=key_id),
-#         'form': form
-#     }
-
-#     title = "tasks"
-#     return render(request, 'nessues_app/tasks.html', {'title': title, 'content': content})
-
 """
  Forms to handle:  
     1) CreateTaskForm
@@ -86,54 +69,19 @@ class TasksView(TemplateView):
     complete_task_class = CompleteTaskForm
 
     def get(self, request, *args, **kwargs): 
-        print(f"Response to GET request: {self.kwargs['key_id']} from {self.request.user.id}")
+        available_tasks = Task.objects.filter(table=self.kwargs['key_id'])
         create = self.create_task_class(initial={'table': self.kwargs['key_id'], 'created_by': self.request.user.id})
         update = self.update_task_class(initial={'table': self.kwargs['key_id'], 'created_by': self.request.user.id})
         complete = self.complete_task_class(initial={'table': self.kwargs['key_id'], 'created_by': self.request.user.id})
 
-        return render(request, self.template_name, {'create': create, 'update': update, 'complete': complete})
+        return render(request, self.template_name, {'available_tasks': available_tasks, 'create': create, 'update': update, 'complete': complete})
 
     def post(self, request, *args, **kwargs):
-        post_data = request.POST or None
-        create_form = self.create_task_class(post_data, prefix='create')
-        # print(f'Current user_id: {request.user.id}\nCurrent room_id: {self.kwargs["key_id"]}')
-        update_form = self.update_task_class(post_data, prefix='update')
-        complete_form = self.complete_task_class(post_data, prefix='complete')
-
-        context = self.get_context_data(create_form=create_form, update_form=update_form, complete_form=complete_form)
+        create_form = self.create_task_class(request.POST)
 
         if create_form.is_valid():
-            self.form_create(create_form)
-        else:
-            messages.error(request, f'Something wrong with the form, here the post data: {request.POST}')
+            create_form.save()
+            # messages.success(request, 'Task Successfully added')
             return HttpResponseRedirect(f'/tables/tasks/{self.kwargs["key_id"]}')
-        if update_form.is_valid():
-            self.form_save(update_form)
-        else: 
-            messages.error(request, f'Something wrong with the form, here the post data: {request.POST}')
-            return HttpResponseRedirect(f'/tables/tasks/{self.kwargs["key_id"]}')
-        if complete_form.is_valid():
-            self.form_save(complete_form)
-        else: 
-            messages.error(request, f'Something wrong with the form, here the post data: {request.POST}')
-            return HttpResponseRedirect(f'/tables/tasks/{self.kwargs["key_id"]}')
-
-    def form_create(self, form): 
-        obj = form.save(commit=False)
-        obj.created_by = request.user.id
-        obj.date_created = datetime.now()
-        obj.save()
-        messages.success(self.request, '{} done!'.format(obj))
-        return HttpResponseRedirect(f'rooms/')
-
-    def form_update(self, form):
-        obj = form.save(commit=False)
-        # obj.
-        messages.success(self.request, '{} done!'.format(obj))
-        return obj
-
-    def form_complete(self, form):
-        obj = form.save(commit=False)
-        # obj.
-        messages.success(self.request, '{} done!'.format(obj))
-        return obj
+        
+        return render(request, self.template_name, {'create': create, 'update': update, 'complete': complete})
