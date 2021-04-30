c8f0ed5a44ab0fb3673397139d87fe4e062e2b83
jol79 <jolhuck@gmail.com>
2021-04-30 10:29:03 +0300

    close #35

--- nessues/static/js/base.js
+++ nessues/static/js/base.js
@@ -3,8 +3,8 @@ $(function(){
     $(".completeTask").click(function(){
         $('.close_task_modal').show();
         // how to get data.id from the template?
-        var task_to_close = $(this).task_id;
-        $(".form-group-id #id_id").val(task_to_close);
+        var id = $(this).data('id');
+        $(".form-group-id #id_id").val(id);
     })
 });
 
